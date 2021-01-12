63d89488501f0299c0c4ad57f35dc4b15c0c97cd
Beatrix Woo <beatrixwho@gmail.com>
2021-01-12 11:30:01 -0800

    add dummy data and render to front page

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
+import { SafeAreaView, ScrollView, Text, View } from 'react-native';
 import HomeScreen from './src/Screens/Home/index'
 import Post from './src/Components/Post/index'
-import Entype from 'react-native-vector-icons/Entypo';
+
 // AWS-AMPLIFY
-// import Amplify from 'aws-amplify'
-// import config from './src/aws-exports'
-// Amplify.configure(config)
+import Amplify from 'aws-amplify'
+import config from './src/aws-exports'
+Amplify.configure(config)
+
+// DUMMY DATA
+import feed from './assets/data/feed'
+const post1 = feed[0]
+const post2 = feed[1]
+const post3 = feed[2]
 
 export default function App() {
   return (
-    <View>
-        {/* <HomeScreen/> */}
-        <Post />
-    </View>
+    <ScrollView>
+      <HomeScreen/>
+      <Post post={post1}/>
+      <Post post={post2}/>
+      <Post post={post3}/>
+    </ScrollView>
   );
 }
--- src/Components/Post/index.jsx
+++ src/Components/Post/index.jsx
@@ -2,27 +2,31 @@ import React from 'react';
 import { View, Text, Image } from 'react-native';
 import styles from './styles';
 
-const Post = (props) => {
+const Post = ({post}) => {
+
+    const pic = post.image
+    const defaultpic = 'https://cdn.dribbble.com/users/2233706/screenshots/8256191/media/9a8a3f67617ea74ae27dd1a0135e8bf5.png'
+
     return (
         <View style={styles.container}>
             {/* Image */}
             <Image style={styles.image}
-            source={{uri: 'https://cdn.dribbble.com/users/2233706/screenshots/8256191/media/9a8a3f67617ea74ae27dd1a0135e8bf5.png?'}}/>
+            source={{uri: pic}}/>
 
             {/* Bed & Bedroom */}
-            <Text style={styles.rooms}>5 bed 3 bedroom</Text>
+            <Text style={styles.rooms}>{post.bed} bed {post.bedroom} bedroom</Text>
 
             {/* Type & Description */}
-            <Text style={styles.overview} numberOfLines={2}>Sugar Pine Escape is a cozy mountain escape that backs up to the National Forest on a less busy portion of the mountain in Sugarloaf, CA. Sugarloaf is a small community that sits just minutes up a small hill above Big Bear City and is only a short distance (approximately 8 miles) from Big Bear Lake, Bear Mountain Ski Resort, Snow Summit Ski Resort and the Village! Take a short drive and enjoy everything that Big Bear has to offer... or if you choose, cuddle up by the fire and escape the crowds.</Text>
+            <Text style={styles.overview} numberOfLines={2}>{post.type}. {post.title}</Text>
 
             {/* Old Price vs Discount Price */}
             <Text style={styles.prices}>
-            <Text style={styles.oldprice}>$50</Text>
-            <Text style={styles.discount}> $40</Text> / night
+            <Text style={styles.oldprice}>${post.oldPrice}</Text>
+            <Text style={styles.discount}> ${post.newPrice}</Text> / night
             </Text>
 
             {/* Final Price */}
-            <Text style={styles.finalprice}>$250 Total</Text>
+            <Text style={styles.finalprice}>${post.totalPrice} Total</Text>
 
         </View>
     )
