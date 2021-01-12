cb1fcee15c6a1c544dd9ecddc2a95837026f638d
gabtorre <gtorresx3@gmail.com>
2021-01-12 13:14:43 -0800

    Adds navigation

--- src/screens/DestinationSearch/index.jsx
+++ src/screens/DestinationSearch/index.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
-import { View, TextInput, Text, FlatList } from 'react-native';
+import { View, TextInput, Text, FlatList, Pressable } from 'react-native';
 import styles from './styles.jsx';
 import searchResults from '../../../assets/data/search';
 import Entypo from 'react-native-vector-icons/Entypo';
+import { useNavigation } from '@react-navigation/native';
 
 const DestinationSearchScreen = () => {
   const [inputText, setInputText] = useState('');
+
+  const navigation = useNavigation();
 
   return (
     <View style={styles.container}>
@@ -19,12 +22,12 @@ const DestinationSearchScreen = () => {
       <FlatList
         data={searchResults}
         renderItem={({item}) => (
-          <View style={styles.row}>
+          <Pressable onPress={() => navigation.navigate('Guests')} style={styles.row}>
             <View style={styles.iconContainer}>
               <Entypo name={"location-pin"} size={30} />
             </View>
             <Text style={styles.locationText}>{item.description}</Text>
-          </View>
+          </Pressable>
         )}
       />
     </View>
