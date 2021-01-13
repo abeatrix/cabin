6d8f282f05f1f810072125e3f45ed97263a4702d
Beatrix Woo <beatrixwho@gmail.com>
2021-01-13 09:31:40 -0800

    add style

--- src/Screens/Inbox/styles.jsx
+++ src/Screens/Inbox/styles.jsx
@@ -5,8 +5,45 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'center',
     },
+    pagetitle: {
+        marginTop: 20,
+        fontWeight: 'bold',
+        fontSize: 20,
+        color: '#071C58',
+    },
+    avatar: {
+        marginTop: 50,
+        borderRadius: 50,
+        width: 100,
+        height: 100
+    },
+    row: {
+        flexDirection: 'row',
+    },
+    rowTitle: {
+        marginHorizontal: 10,
+        marginTop: 20,
+        fontWeight: 'bold',
+        color: '#2D57A7',
+    },
+    col: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center'
+    },
+    rowText: {
+        fontSize: 12,
+        padding: 10,
+        color: '#071C58'
+    },
+    inbox: {
+        margin: 5,
+        backgroundColor: '#94B7D7',
+        flex: 1,
+        width: '100%',
+        paddingVertical: 30
+    }
 });
 
 export default styles;
--- src/Screens/Profile/index.jsx
+++ src/Screens/Profile/index.jsx
@@ -1,16 +1,48 @@
 import React from 'react'
-import { SafeAreaView, Text, View } from 'react-native'
+import { SafeAreaView, Text, View, Image, FlatList } from 'react-native'
 import styles from './styles';
+import PostCarouselItem from '../../Components/Post/PostCarouselItem'
+//DUMMY DATA
+import places from '../../../assets/data/feed'
+import useWindowDimensions from 'react-native/Libraries/Utilities/useWindowDimensions';
 
-const ProfileScreen = () => {
+const ProfileScreen = (props) => {
+    const width = useWindowDimensions().width;
+
     return (
         <SafeAreaView style={styles.container}>
-            <Text style={styles.fullname}>Gabe Torres</Text>
+            <Image style={styles.avatar} source={{uri: 'https://i.ibb.co/cJ6G9Vc/image.png'}}/>
+            <Text style={styles.fullname}>Bruno Torres</Text>
             <Text>Edit Profile</Text>
+            <Text style={styles.about}>Enjoy sharing my vacation with travlers and introduce them to local culture.</Text>
             <View style={styles.row}>
-                <Text style={styles.rowTitle}>LANGUAGE</Text>
-                <Text style={styles.rowTitle}>LOCATION</Text>
+                <View style={styles.col}>
+                    <Text style={styles.rowTitle}>LANGUAGE</Text>
+                    <Text style={styles.rowText}>English, French</Text>
+                </View>
+                <View style={styles.col}>
+                    <Text style={styles.rowTitle}>LOCATION</Text>
+                    <Text style={styles.rowText}>Los Angeles, CA</Text>
+                </View>
+                <View style={styles.col}>
+                    <Text style={styles.rowTitle}>MEMBER SINCE</Text>
+                    <Text style={styles.rowText}>2020</Text>
+                </View>
             </View>
+
+            {/* Listings */}
+            <Text style={styles.rowTitle}>LISTINGS</Text>
+            <View style={styles.carousel}>
+                <FlatList
+                data={places}
+                renderItem={({item}) => <PostCarouselItem key={item.id} post={item} />}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                snapToInterval={width - 60}
+                snapToAlignment={"center"}
+                decelerationRate={'fast'}
+                />
+          </View>
         </SafeAreaView>
     )
 }
